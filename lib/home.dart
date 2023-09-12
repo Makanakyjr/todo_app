605560d23539adeb7d5018435069eb8db0c6a335
Makanakyjr <olayinkakenahmed@gmail.com>
2023-09-12 15:30:49 +0100

    ToDo app

---
+++ lib/home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(131, 239, 242, 243),
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: Colors.black,
+              size: 30,
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/yak.jpg')),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 239, 228, 228),
+                  borderRadius: BorderRadius.circular(20)),
+              child: TextField(
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(0),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                        size: 20,
+                      ))),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
