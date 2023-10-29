61522c98c9b1f309955845e7e318559df3277f9d
Makanakyjr <olayinkakenahmed@gmail.com>
2023-10-29 19:10:44 +0100

    forgot passwrrd

--- lib/widgets/register_page.dart
+++ lib/widgets/register_page.dart
@@ -73,7 +73,7 @@ class _RegisterPageState extends State<RegisterPage> {
                 if (context.mounted) {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => Home()),
+                    MaterialPageRoute(builder: (context) => const Home()),
                   );
                 }
               },
