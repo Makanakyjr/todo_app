5f1ec68b184f4478bf7936135027c304c08735c8
Makanakyjr <olayinkakenahmed@gmail.com>
2023-10-29 20:36:01 +0100

    cloud_firestore

--- lib/widgets/register_page.dart
+++ lib/widgets/register_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/home.dart';
 import 'package:todo_app/model/user.dart';
-// import 'package:todo_app/login_page.dart';
 import 'package:todo_app/signin_page.dart';
 import 'package:todo_app/textfield_todo.dart';
 
@@ -23,92 +22,94 @@ class _RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            const Icon(
-              Icons.lock_clock_sharp,
-              size: 100,
-            ),
-            const SizedBox(height: 50),
-            const Text(
-              'lets create an account for you!',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 10,
               ),
-            ),
-            const SizedBox(height: 20),
-            MyTextField(
-              controller: email,
-              hintText: ('Email'),
-              obsecureText: false,
-            ),
-            const SizedBox(height: 40),
-            MyTextField(
-              controller: password,
-              hintText: ('Password'),
-              obsecureText: true,
-            ),
-            const SizedBox(height: 40),
-            MyTextField(
-              controller: confirmPassword,
-              hintText: ('Confirm Password'),
-              obsecureText: true,
-            ),
-            const SizedBox(height: 10),
-            const SizedBox(
-              height: 50,
-            ),
-            SignInPage(
-              onTap: () async {
-                await signUp(
-                    email: email.text.trim(),
-                    password: password.text.trim(),
-                    username: "Ahmed");
-
-                if (context.mounted) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Home()),
-                  );
-                }
-              },
-            ),
-            const SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Already a member? ',
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black),
+              const Icon(
+                Icons.lock_clock_sharp,
+                size: 100,
+              ),
+              const SizedBox(height: 50),
+              const Text(
+                'lets create an account for you!',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
                 ),
-                const SizedBox(width: 4),
-                GestureDetector(
-                  onTap: () {
+              ),
+              const SizedBox(height: 20),
+              MyTextField(
+                controller: email,
+                hintText: ('Email'),
+                obsecureText: false,
+              ),
+              const SizedBox(height: 40),
+              MyTextField(
+                controller: password,
+                hintText: ('Password'),
+                obsecureText: true,
+              ),
+              const SizedBox(height: 40),
+              MyTextField(
+                controller: confirmPassword,
+                hintText: ('Confirm Password'),
+                obsecureText: true,
+              ),
+              const SizedBox(height: 10),
+              const SizedBox(
+                height: 50,
+              ),
+              SignInPage(
+                onTap: () async {
+                  await signUp(
+                      email: email.text.trim(),
+                      password: password.text.trim(),
+                      username: "Ahmed");
+
+                  if (context.mounted) {
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => const Home()),
                     );
-                  },
-                  child: const Text(
-                    'Log In',
+                  }
+                },
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Already a member? ',
                     style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: Color.fromARGB(255, 88, 149, 240),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black),
+                  ),
+                  const SizedBox(width: 4),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Home()),
+                      );
+                    },
+                    child: const Text(
+                      'Log In',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Color.fromARGB(255, 88, 149, 240),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
